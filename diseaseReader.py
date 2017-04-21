3837f9076cebbd1205d962c0a9354920bb02990c
Isaiah Weaver <iswea1@morgan.edu>
2017-04-21 10:33:06 -0400

    update

--- diseaseReader.py
+++ diseaseReader.py
@@ -1,3 +1,9 @@
 from nltk.corpus.reader import WordListCorpusReader
+#This code is supposed to actually read the word list of diseases
+#and show the words correctly in the console. 
+#This line uses WordListCorpusReader and sets equal to our wordlist
 reader = WordListCorpusReader('.', ['wordlist'])
+#Function grabs the disease words
+#The code works with no errors however, the words aren't being printed. 
+#I'm not sure why since my wordlist is a text file.
 reader.words()

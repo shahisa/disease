f102ff5f3ed8bc1d261faaac98e140937e1569a2
Isaiah Weaver <iswea1@morgan.edu>
2017-04-21 09:58:16 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@
 # this is the setup.py file. This is for creating the pip 
 # module
 
+name = 'disease'
+version = '0.0.1'
+description = 'A python extractor that takes disease names from URL or text'
+url = "https://github.com/shahisa/disease"
+
 setup()
